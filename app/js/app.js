d2ae51963494972833fd5e34663e32a475df7b2a
matsiakh <665lucifer1@gmail.com>
2023-03-15 18:29:37 +0200

    header js

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+
+	const hamburger = (ham, leng, active, hamActive, header, headerActive) => {
+			const button = document.querySelector(ham),
+					headerLeng = document.querySelector(leng),
+					head = document.querySelector(header);
+					console.log(head);
+
+				button.addEventListener('click', () => {
+					headerLeng.classList.toggle(active);
+					button.classList.toggle(hamActive);
+					head.classList.toggle(headerActive);
+				});
+
+	};
+	hamburger('.hamburger', '.menu', 'menu--active', 'hamburger--active', '.header', 'header--menu');
+
+
+
+
+
+})
+
+
